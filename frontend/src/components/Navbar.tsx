92830c360c71e16392d433cf784ce1a4b60320b6
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-09-02 12:54:00 +0200

    Setup Navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import type {UserObject} from "../types/User.ts";
+
+type NavbarProps = {
+    user : UserObject;
+    login: () => void;
+    logout: ()=>void;
+}
+
+export function Navbar({user,login,logout}:NavbarProps) {
+    let returnContent;
+    if(user===undefined){
+        returnContent = <button onClick={login} id={"loginbutton"}>Login</button>;
+    }else{
+        returnContent =
+            <>
+                <img src={user.avatar_url} id={"userMenuAvatar"} />
+                <div id={"hiddenMenu"}>
+                    <button onClick={logout}>Logout</button>
+                </div>
+            </>;
+    }
+
+
+    return (
+        <nav>
+            <img src={"../src/assets/150xRaptors_contour.png"} id={"navIcon"} />
+            <ul id={"navMenu"}>
+                <li>
+                    <a href={"/"}>Home</a>
+                </li>
+                <li>
+                    <a href={"/turniere"}>Turniere</a>
+                </li>
+            </ul>
+            <div id={"userMenu"}>
+            {returnContent}
+            </div>
+        </nav>
+    )
+}
