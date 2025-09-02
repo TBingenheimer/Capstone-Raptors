b4458e050c9628262afe44220fd38260c80090f4
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-09-02 16:57:39 +0200

    Cleaned TorunamendCard

--- frontend/src/components/TournamentCard.tsx
+++ frontend/src/components/TournamentCard.tsx
@@ -1,32 +1,15 @@
 import type {TournamentObject} from "../types/Tournament.ts";
 
 type TournamentProps = {
-    tournament : UserObject;
+    tournament : TournamentObject;
 }
 
 export function TournamentCard({tournament}:TournamentProps) {
-    let mockTurnierListe:TournamentObject[] = [
-        {
-            name : "Turnier zu Bochum",
-            startDate : "2025-09-29",
-            endDate : "2025-09-31",
-            description : "Tolles Turnier in Bochum",
-            participants : 5,
-        },
-        {
-            name : "Turnier zu Münster",
-            startDate : "2025-09-29",
-            endDate : "2025-09-31",
-            description : "Tolles Turnier in Bochum",
-            participants : 5,
-        }
-    ];
+
 
     return (
         <>
-            {mockTurnierListe.map((turnier:TournamentObject) => {
-                <TournamentCard  />
-            })}
+
         </>
     );
 }
