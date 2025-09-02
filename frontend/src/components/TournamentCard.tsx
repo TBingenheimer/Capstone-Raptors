29d08998f70351229b20e710ea7f71f710cc7283
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-09-02 19:54:38 +0200

    Added Data to Card

--- frontend/src/components/TournamentCard.tsx
+++ frontend/src/components/TournamentCard.tsx
@@ -1,15 +1,16 @@
-import type {TournamentObject} from "../types/Tournament.ts";
+import type { TournamentObject } from "../types/Tournament.ts";
+import {routerConfig} from "../routerConfig.ts";
 
-type TournamentProps = {
-    tournament : TournamentObject;
-}
+type TournamentCardProps = {
+    tournament: TournamentObject;
+};
 
-export function TournamentCard({tournament}:TournamentProps) {
-
-
+export function TournamentCard({ tournament }: TournamentCardProps) {
     return (
-        <>
-            {tournament.name}
-        </>
+        <div className="tournament-card">
+            <h3>{tournament.name}</h3>
+            <p>{tournament.startDate}</p>
+            <a href={routerConfig.URL.TURNIER(tournament.name)}>Details</a>
+        </div>
     );
 }
