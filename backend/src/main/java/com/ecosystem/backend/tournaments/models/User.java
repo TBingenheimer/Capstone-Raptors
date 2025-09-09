f8a4e0fa9e2f1a9c84d301bad6a61ff72947b4e8
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-09-09 10:35:04 +0200

    Create User Model

--- backend/src/main/java/com/ecosystem/backend/tournaments/models/User.java
+++ backend/src/main/java/com/ecosystem/backend/tournaments/models/User.java
@@ -1,4 +1,7 @@
 package com.ecosystem.backend.tournaments.models;
 
-public record User(String id, String name, String displayName, String avatar_url,String jesrseyNumber) {
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("user")
+public record User(String id, String displayName,String jesrseyNumber, String avatar_url) {
 }
