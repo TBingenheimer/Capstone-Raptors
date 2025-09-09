8188a0ece022594d25d0de5638de7702c4c86975
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-09-09 19:03:48 +0200

    Created tests

---
+++ backend/src/test/java/com/ecosystem/backend/controller/TournamentControllerTest.java
@@ -0,0 +1,19 @@
+package com.ecosystem.backend.controller;
+
+class TournamentControllerTest {
+    /*@Test
+    void getAllTournaments_Test() {
+        Tournament tournament1 = new Tournament("1","9. Turnier zu Münster","Tolles Turnier in Münster","2025-07-27","2025-07-29",7, Arrays.asList("2","3","4","5","6"));
+        Tournament tournament2 = new Tournament("2","4. Bochumer Sommerturnier","Tolles Turnier in Bpochum","2025-08-29","2025-08-31",7, Arrays.asList("2","3","4","5","6"));
+        Map<String, Tournament> tournaments = new TreeMap();
+        tournaments.put("1",tournament1);
+        tournaments.put("2",tournament2);
+
+        assertEquals(getAllTournaments(), tournaments);
+    }
+    @Test
+    void getTournamentById_Test() {
+        Tournament tournament = new Tournament("1","9. Turnier zu Münster","Tolles Turnier in Münster","2025-07-27","2025-07-29",7, Arrays.asList("2","3","4","5","6"));
+        assertEquals(getTournament(1), tournament);
+    }*/
+}
---
+++ backend/src/test/java/com/ecosystem/backend/controller/UserControllerTest.java
@@ -0,0 +1,46 @@
+package com.ecosystem.backend.controller;
+
+import com.ecosystem.backend.repository.UserRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureMockRestServiceServer
+class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepo userRepo;
+
+
+    @Test
+    void getUserById_should_return_default_user() throws Exception {
+        mockMvc.perform(get("/api/user/getUser/0"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                    {
+                        "id":"0",
+                        "displayName":"Demo User",
+                        "jerseyNumber":"0",
+                        "avatar_url":"http://localhost:5173/src/assets/free.png"
+                    }
+                    """));
+    }
+
+    @Test
+    void getAllUsers_should_return_all_users() throws Exception {
+        mockMvc.perform(get("/api/user/getAllUsers"))
+                .andExpect(status().isOk());
+    }
+
+}
