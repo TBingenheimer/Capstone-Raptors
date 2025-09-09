6f4e2a41e9fdd0b5da662f4214e0da3cf3febfe3
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-09-09 10:37:15 +0200

    Add axios calls to overview, styling

--- frontend/src/pages/TournamentOverviewPage.tsx
+++ frontend/src/pages/TournamentOverviewPage.tsx
@@ -2,12 +2,23 @@ import type {UserObject} from "../types/User.ts";
 import type {TournamentObject} from "../types/Tournament.ts";
 import {TournamentCard} from "../components/TournamentCard.tsx";
 import "../styles/TournamentOverview.css";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {map} from "react-bootstrap/ElementChildren";
 
 type turnierProps = {
     user : UserObject;
 }
 
 export function TournamentOverviewPage({user}:turnierProps) {
+    const [tournaments, setTournaments] = useState<TournamentObject[]>([]);
+    useEffect(() => {
+        axios.get("/api/tournament/getAllTournaments")
+            .then((response)=>{
+                setTournaments(response.data.tournaments);
+            });
+    },[])
+    console.log(tournaments);
     let mockTurnierListe = [
         {
             name : "Turnier zu Bochum",
@@ -26,10 +37,10 @@ export function TournamentOverviewPage({user}:turnierProps) {
     ];
 
     return (
-        <div className="tournament-card-wrap">
+        <div className="tournament-card-wrap contentWrap">
             <link href={"../src/styles/TournamentOverview.css"} rel="stylesheet" />
             <h1 style={{width:"100%"}}>Offene Turniere</h1>
-            {mockTurnierListe.map((tournament:TournamentObject)=> {
+            {tournaments.map((tournament:TournamentObject)=> {
                 return <TournamentCard key={tournament.name} tournament={tournament}  />
             })}
         </div>
