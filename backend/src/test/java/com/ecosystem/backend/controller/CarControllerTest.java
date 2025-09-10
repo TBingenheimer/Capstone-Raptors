2c619d0fb36d2dd50aa44e193568cf8f9d7d06ef
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-09-10 12:01:38 +0200

    Removed unused import

--- backend/src/test/java/com/ecosystem/backend/controller/CarControllerTest.java
+++ backend/src/test/java/com/ecosystem/backend/controller/CarControllerTest.java
@@ -8,11 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockR
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
