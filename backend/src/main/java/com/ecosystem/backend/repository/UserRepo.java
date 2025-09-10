413ea0269225f7b95788bda1fa6a3541c34dc4aa
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-09-10 09:32:28 +0200

    Changed orElse to orElseGet to always throw Demo User to not get 404

--- backend/src/main/java/com/ecosystem/backend/repository/UserRepo.java
+++ backend/src/main/java/com/ecosystem/backend/repository/UserRepo.java
@@ -1,6 +1,6 @@
 package com.ecosystem.backend.repository;
 
-import com.ecosystem.backend.tournaments.models.User;
+import com.ecosystem.backend.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepo extends MongoRepository<User,String> {
