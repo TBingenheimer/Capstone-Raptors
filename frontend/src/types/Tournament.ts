26fc23e50722b1102a5c930f5d38d1b793d08962
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-09-11 10:53:30 +0200

    Changed order of attributes to adhere to same schema as db and model

--- frontend/src/types/Tournament.ts
+++ frontend/src/types/Tournament.ts
@@ -1,12 +1,12 @@
 export type TournamentObject = {
     id:string,
     name : string,
+    description : string,
     street : string,
     zip : string,
     city : string,
     startDate : string,
     endDate : string,
-    description : string,
     participants : number,
 }
 
