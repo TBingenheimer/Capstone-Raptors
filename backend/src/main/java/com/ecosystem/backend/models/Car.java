819794cfa7c0b100afc16f6dc89d4963352c2102
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-09-16 09:34:53 +0200

    Test für Sonar

--- backend/src/main/java/com/ecosystem/backend/models/Car.java
+++ backend/src/main/java/com/ecosystem/backend/models/Car.java
@@ -1,10 +1,12 @@
 package com.ecosystem.backend.models;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 @Document("cars")
 public record Car(
+        @Id String id,
         String driverId,
         String tournamentId,
         String availableSeats,
---
+++ frontend/src/components/TournamentForm.tsx
@@ -0,0 +1,53 @@
+import {type ChangeEvent, useState} from "react";
+import type {Tournament} from "../types/Tournament.ts";
+
+export function TournamentForm(props) {
+    const [tournament, setTournament] = useState<Tournament>({
+        id:crypto.randomUUID(),
+        name:"",
+        description:"",
+        street:"",
+        zip:"",
+        city:"",
+        startDateTime:"",
+        endDateTime:"",
+        participants:0
+    });
+
+    function setValues(e:ChangeEvent<HTMLInputElement>){
+        setTournament(prev=>({
+            ...prev,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    return (
+        <div id={"tournamentCreate"} onSubmit={sendData} className={"contentWrap"}>
+            <h1>Neues Turnier anlegen</h1>
+            <form id={"tournamentCreateForm"}>
+                <label>Turniername: <br />
+                    <input type={"text"} id={"name"} name={"name"} value={tournament.name} onChange={setValues} />
+                </label>
+                <label>Strasse:<br />
+                    <input type={"text"} name={"street"} value={tournament.street}  onChange={setValues}/>
+                </label>
+                <label>PLZ:<br />
+                    <input type={"text"} name={"zip"} value={tournament.zip}  onChange={setValues}/>
+                </label>
+                <label>Ort:<br />
+                    <input type={"text"} name={"city"} value={tournament.city}  onChange={setValues}/>
+                </label>
+                <label>Start: <br />
+                    <input type={"datetime-local"} id={"tournamentStartTime"} name={"startDateTime"} value={tournament.startDate}  onChange={setValues}/>
+                </label>
+                <label>Ende: <br />
+                    <input type={"datetime-local"} id={"tournamentEndTime"} name={"endDateTime"} value={tournament.endDate}  onChange={setValues}/>
+                </label>
+                <label>Beschreibung: <br />
+                    <textarea id={"tournamentDescription"} name={"description"}  onChange={setValues} value={tournament.description} />
+                </label>
+                <input type={"submit"} id={"tournamentSubmit"} value={"Speichern"} />
+            </form>
+        </div>
+    )
+}
